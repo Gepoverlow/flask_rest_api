65da4cd3ace7d7c17577f91e476c508d2ae0859c
lucas <gepeoverload@gmail.com>
2023-03-23 11:59:59 +0100

    tidied init up and added some comments

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,10 @@ from flask import Flask
 from app import main
 from app.main.database import db
 from app.main.api import api
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from spyne.server.wsgi import WsgiApplication
 from app import model
 
 
+# BASE APP & ADDING REST FUNCTIONALITY ON API
 def create_app():
     app = Flask(__name__)
     app.config.from_object(main.settings[os.environ.get('APPLICATION_ENV', 'default')])
@@ -20,14 +19,19 @@ def create_app():
     return app
 
 
+# INITIALIZING APP
 app = create_app()
 
 
+# CREATING TABLES BASED ON THE MODELS
 with app.app_context():
     db.create_all()
 
 
+# ADDING SOAP FUNCTIONALITY
 from app.soap import spyne
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from spyne.server.wsgi import WsgiApplication
 
 # SOAP services are distinct wsgi applications, we should use dispatcher
 # middleware to bring all aps together
@@ -35,6 +39,8 @@ app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
     '/soap': WsgiApplication(spyne.create_app())
 })
 
+
+# ADDING GRAPHQL FUNCTIONALITY
 from app.graphql.schema import schema
 from flask_graphql import GraphQLView
 
